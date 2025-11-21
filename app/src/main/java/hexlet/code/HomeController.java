d015df8659f7bb6b84178d3e5cd20eb3356cb415
Grigoriy <g.karpov@it-alnc.ru>
2025-11-21 17:05:45 +0300

    add blog project

---
+++ app/src/main/java/hexlet/code/HomeController.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@GetMapping("/")
+	public String home() {
+		return "Добро пожаловать в мой блог!";
+	}
+}
